919b4ad27b759ace36622dadd16504f843445d0c
hackerhgl <hackerhgl@gmail.com>
2021-05-15 11:51:16 +0500

    Basic grid & bomb siblings calculations

---
+++ main.go
@@ -0,0 +1,84 @@
+//go 1.10.4
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	r := 9
+	c := 9
+	moves := 0
+	finish := false
+
+	// grid := [r+1][c+1]int{}
+	grid := make([][]int, r+1)
+	steps := make([][]int, r+1)
+
+	for ri := 0; ri <= r; ri++ {
+		grid[ri] = make([]int, c+1)
+		steps[ri] = make([]int, c+1)
+		for ci := 0; ci <= c; ci++ {
+			rand.Seed(time.Now().UnixNano() + int64(ri+ci))
+			random := rand.Intn(5)
+			// fmt.Println("Check %d", random)
+			if random == 1 {
+				grid[ri][ci] = -1
+			}
+		}
+	}
+	for ri := 0; ri <= r; ri++ {
+		for ci := 0; ci <= c; ci++ {
+			if grid[ri][ci] == -1 {
+				continue
+			}
+			count := 0
+			// Here we compute
+			fc := ci > 0
+			lc := ci < c
+			fr := ri > 0
+			lr := ri < r
+			pci := ci - 1
+			pri := ri - 1
+			nci := ci + 1
+			nri := ri + 1
+			if fc && fr && grid[pri][pci] == -1 {
+				count++
+			}
+			if fc && grid[ri][pci] == -1 {
+				count++
+			}
+			if fc && lr && grid[nri][pci] == -1 {
+				count++
+			}
+			if fr && grid[pri][ci] == -1 {
+				count++
+			}
+			if lr && grid[nri][ci] == -1 {
+				count++
+			}
+			if fr && lc && grid[pri][nci] == -1 {
+				count++
+			}
+			if lc && grid[ri][nci] == -1 {
+				count++
+			}
+			if lc && lr && grid[nri][nci] == -1 {
+				count++
+			}
+			grid[ri][ci] = count
+		}
+	}
+
+	//fmt.Println(grid)
+
+	for !finish {
+		moves++
+	}
+
+	fmt.Println("Helloww World")
+
+}
